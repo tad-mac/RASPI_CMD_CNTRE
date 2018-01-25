5a170f3cfb42a8cfca4e372c7d1258da06f485ec
Tadeu Machado <tadeumachado@outlook.com>
2018-01-24 20:59:48 -0500

    V0.1.1
    Restructed the program to add more functions and a way to seperate the stylesheet

--- RASPI_COMMAND_CNTRE/main.py
+++ RASPI_COMMAND_CNTRE/main.py
@@ -1,12 +1,17 @@
+import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 
-class Page(QWidget):
-    def __init__(self, parent=None):
-        super(Page, self).__init__(parent)
+class MainWindow(QWidget):
 
-        self.my_label = QLabel("RASPI")
+    def __init__(self):
+        super().__init__()
+        # self.style_from_file()
+        self.init_ui()
+
+    def init_ui(self):
+        self.title = QLabel("RASPI")
         self.showTime = QLabel("")
         timer = QTimer(self)
         timer.timeout.connect(self.timing)
@@ -14,26 +19,25 @@ class Page(QWidget):
 
         self.timing()
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.my_label)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.title)
         self.layout.addWidget(self.showTime)
-        mainLayout = QGridLayout()
-        mainLayout.addLayout(self.layout, 0, 1)
-        self.setLayout(mainLayout)
-
+        self.setLayout(self.layout)
         self.setWindowTitle("RASPI CMD CNTR")
+        self.show()
+
+    def style_from_file(path):
+        self.setObjectName("MainWidget")
+        with open(path, 'r') as f:
+            content = f.read()
+        return content
 
     def timing(self):
         time = QTime.currentTime().toString('hh:mm:ss')
         self.showTime.setText(time)
 
 
-if __name__ == "__main__":
-
-    import sys
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    window = Page()
-    window.show()
-
+    window = MainWindow()
     sys.exit(app.exec_())
